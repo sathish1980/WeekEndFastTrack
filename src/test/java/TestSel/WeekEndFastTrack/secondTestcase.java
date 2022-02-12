384d10676d928514195d290369f50d54721110b4
sathish1980 <kumar.sathish189@gmail.com>
2022-02-12 14:57:05 +0530

    New file

--- src/test/java/TestSel/WeekEndFastTrack/secondTestcase.java
+++ src/test/java/TestSel/WeekEndFastTrack/secondTestcase.java
@@ -2,6 +2,10 @@ package TestSel.WeekEndFastTrack;
 
 public class secondTestcase {
 
+	public void sample()
+	{
+		System.out.println("This is done by sathish");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
